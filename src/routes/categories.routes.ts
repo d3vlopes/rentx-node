e054733e85d179621988e1f4a18b727b7fcd4c06
Leandro Lopes <leandro-lopes46@hotmail.com>
2022-01-04 20:45:11 -0300

    feat: add category use case

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,14 @@
 import { Request, Response, Router } from 'express'
 
 import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository'
-import { CreateCategory } from '../modules/cars/services/CreateCategory'
+import { createCategoryController } from '../modules/cars/useCases/createCategory'
 
 export const categoriesRoutes = Router()
 
 const categoriesRepository = new CategoriesRepository()
 
 categoriesRoutes.post('/', (req: Request, res: Response) => {
-  const { name, description } = req.body
-  const createCategoryService = new CreateCategory(categoriesRepository)
-
-  createCategoryService.execute({ name, description })
-
-  return res.status(201).send()
+  return createCategoryController.handle(req, res)
 })
 
 categoriesRoutes.get('/', (req: Request, res: Response) => {
