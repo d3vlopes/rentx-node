ed758d1f1790a88335eb56b9188cbf3073f15354
Leandro Lopes <leandro-lopes46@hotmail.com>
2022-01-04 14:39:43 -0300

    test: config debugger

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,10 @@ import express from 'express'
 
 const app = express()
 
+app.post('/courses', (req, res) => {
+  const { name } = req.body
+
+  return res.json({ name })
+})
+
 app.listen(3333, () => console.log('Server is running!'))
