6bdebff2124b72b5e5c559d4c40293401cb65367
Leandro Lopes <leandro-lopes46@hotmail.com>
2022-01-05 16:16:23 -0300

    feat: add stream on ImportCategoryUseCase

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,5 +1,16 @@
+import fs from 'fs'
+import { parse } from 'csv-parse'
+
 export class ImportCategoryUseCase {
   execute(file: Express.Multer.File) {
-    console.log(file)
+    const stream = fs.createReadStream(file.path)
+
+    const parseFile = parse()
+
+    stream.pipe(parseFile)
+
+    parseFile.on('data', async (line) => {
+      console.log(line)
+    })
   }
 }
