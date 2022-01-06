784731860c0f6058998a605fb19286ca2e50bd86
Leandro Lopes <leandro-lopes46@hotmail.com>
2022-01-06 20:49:21 -0300

    fix: change type return of Category

--- src/modules/cars/useCases/createCategory/createCategoryController.ts
+++ src/modules/cars/useCases/createCategory/createCategoryController.ts
@@ -4,10 +4,10 @@ import { CreateCategoryUseCase } from './CreateCategoryUseCase'
 export class CreateCategoryController {
   constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
 
-  handle(req: Request, res: Response) {
+  async handle(req: Request, res: Response) {
     const { name, description } = req.body
 
-    this.createCategoryUseCase.execute({ name, description })
+    await this.createCategoryUseCase.execute({ name, description })
 
     return res.status(201).send()
   }
