0a61f2b5d15011da7013d474205fb1c76ea102f5
Leandro Lopes <leandro-lopes46@hotmail.com>
2022-01-07 15:50:48 -0300

    fix: CreateUsers migration

--- src/database/migrations/1641575399917-CreateUsers.ts
+++ src/database/migrations/1641575399917-CreateUsers.ts
@@ -2,7 +2,7 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm'
 
 export class CreateUsers1641575399917 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    queryRunner.createTable(
+    await queryRunner.createTable(
       new Table({
         name: 'users',
         columns: [
