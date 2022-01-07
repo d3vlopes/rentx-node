665bd4c5166c2e6b04f9fc78194e2907da3102a1
Leandro Lopes <leandro-lopes46@hotmail.com>
2022-01-07 20:48:41 -0300

    feat: add verification user exists on CreateUserUseCase

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -12,7 +12,14 @@ export class CreateUserUseCase {
   ) {}
 
   async execute({ name, email, driver_license, password }: ICreateUserDTO) {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email)
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists')
+    }
+
     const passwordHash = await hash(password, 8)
+
     await this.usersRepository.create({
       name,
       email,
