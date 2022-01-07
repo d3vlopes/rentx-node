e351a32bb6f62d727cfe8b55823c505c2ba0225f
Leandro Lopes <leandro-lopes46@hotmail.com>
2022-01-07 16:08:50 -0300

    feat: encrypt user password on CreateUserUseCase

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'tsyringe'
+import { hash } from 'bcrypt'
 
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO'
 import { IUsersRepository } from '../../repositories/IUsersRepository'
@@ -11,11 +12,12 @@ export class CreateUserUseCase {
   ) {}
 
   async execute({ name, email, driver_license, password }: ICreateUserDTO) {
+    const passwordHash = await hash(password, 8)
     await this.usersRepository.create({
       name,
       email,
       driver_license,
-      password,
+      password: passwordHash,
     })
   }
 }
