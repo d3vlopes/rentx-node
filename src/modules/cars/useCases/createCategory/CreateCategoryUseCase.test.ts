43a0b5861949ff6352f62f704ee3feea013defd2
Leandro Lopes <leandro-lopes46@hotmail.com>
2022-01-10 16:18:49 -0300

    test: add test of CreateCategoryUseCase

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.test.ts
@@ -0,0 +1,50 @@
+import { AppError } from '../../../../errors/AppError'
+import { CategoriesRepositoryMock } from '../../repositories/mocks/CategoriesRepositoryMock'
+import { CreateCategoryUseCase } from './CreateCategoryUseCase'
+
+let createCategoryUseCase: CreateCategoryUseCase
+let categoriesRepositoryMock: CategoriesRepositoryMock
+
+describe('Create Category', () => {
+  beforeEach(() => {
+    categoriesRepositoryMock = new CategoriesRepositoryMock()
+    createCategoryUseCase = new CreateCategoryUseCase(categoriesRepositoryMock)
+  })
+
+  it('should be able to create a new category', async () => {
+    const category = {
+      name: 'Category Test',
+      description: 'Category description Test',
+    }
+
+    await createCategoryUseCase.execute({
+      name: category.name,
+      description: category.description,
+    })
+
+    const categoryCreated = await categoriesRepositoryMock.findByName(
+      category.name,
+    )
+
+    expect(categoryCreated).toHaveProperty('id')
+  })
+
+  it('should not be able to create a new category with name exists', async () => {
+    expect(async () => {
+      const category = {
+        name: 'Category Test',
+        description: 'Category description Test',
+      }
+
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      })
+
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      })
+    }).rejects.toBeInstanceOf(AppError)
+  })
+})
