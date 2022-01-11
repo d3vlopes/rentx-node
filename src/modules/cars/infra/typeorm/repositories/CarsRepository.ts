083e0bd87a9cdeba712467258ac0038ab4324a29
Leandro Lopes <leandro-lopes46@hotmail.com>
2022-01-11 20:44:24 -0300

    feat: add method findAvailable on CarsRespository

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -40,4 +40,28 @@ export class CarsRepository implements ICarsRepository {
 
     return car
   }
+
+  async findAvailable(brand?: string, category_id?: string, name?: string) {
+    const carsQuery = await this.repository
+      .createQueryBuilder('c')
+      .where('available = :available', { available: true })
+
+    if (brand) {
+      carsQuery.andWhere('brand = :brand', { brand })
+    }
+
+    if (name) {
+      carsQuery.andWhere('name = :name', { name })
+    }
+
+    if (category_id) {
+      carsQuery.andWhere('category_id = :category_id', {
+        category_id,
+      })
+    }
+
+    const cars = await carsQuery.getMany()
+
+    return cars
+  }
 }
