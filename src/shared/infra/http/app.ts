cf49b4994e51aaa05141f2919805617e9ffa6a0a
Leandro Lopes <leandro-lopes46@hotmail.com>
2022-01-13 18:31:30 -0300

    build: config supertest

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,36 @@
+import 'reflect-metadata'
+import express, { Request, Response, NextFunction } from 'express'
+import 'express-async-errors'
+import swaggerUi from 'swagger-ui-express'
+
+import swaggerFile from '../../../swagger.json'
+
+import { AppError } from '../../errors/AppError'
+
+import createConnection from '../typeorm'
+
+import '../../container'
+
+import { router } from './routes'
+
+createConnection()
+export const app = express()
+
+app.use(express.json())
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
+app.use(router)
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      message: error.message,
+    })
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: `Internal server error - ${error.message}`,
+  })
+})
