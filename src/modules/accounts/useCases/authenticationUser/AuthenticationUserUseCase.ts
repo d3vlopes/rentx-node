bf74d3c944e4dc3b4a314e52709259a521164d45
Leandro Lopes <leandro-lopes46@hotmail.com>
2022-01-14 15:54:29 -0300

    feat: add refresh token generation on AuthenticationUserUseCase

--- src/modules/accounts/useCases/authenticationUser/AuthenticationUserUseCase.ts
+++ src/modules/accounts/useCases/authenticationUser/AuthenticationUserUseCase.ts
@@ -4,6 +4,9 @@ import { sign } from 'jsonwebtoken'
 
 import { IUsersRepository } from '../../repositories/IUsersRepository'
 import { AppError } from '../../../../shared/errors/AppError'
+import { IUsersTokensRepository } from '../../repositories/IUsersTokensRepository'
+import { auth } from '../../../../config/auth'
+import { IDateProvider } from '../../../../shared/container/providers/DateProvider/IDateProvider'
 
 interface IRequest {
   email: string
@@ -16,6 +19,7 @@ interface IResponse {
     email: string
   }
   token: string
+  refresh_token: string
 }
 
 @injectable()
@@ -23,6 +27,10 @@ export class AuthenticationUserUseCase {
   constructor(
     @inject('UsersRepository')
     private UsersRepository: IUsersRepository,
+    @inject('UsersTokensRepository')
+    private usersTokensRepository: IUsersTokensRepository,
+    @inject('DayjsDateProvider')
+    private dateProvider: IDateProvider,
   ) {}
 
   async execute({ email, password }: IRequest): Promise<IResponse> {
@@ -39,9 +47,24 @@ export class AuthenticationUserUseCase {
       throw new AppError('Email or password incorrect!')
     }
 
-    const token = sign({}, 'cfe275a5908b5650488e0b0342c2d6cc', {
+    const token = sign({}, auth.secret_token, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn: auth.expires_in_token,
+    })
+
+    const refresh_token = sign({ email }, auth.secret_refresh_token, {
+      subject: user.id,
+      expiresIn: auth.expires_in_refresh_token,
+    })
+
+    const refresh_token_expires_date = this.dateProvider.addDays(
+      auth.expires_refresh_token_days,
+    )
+
+    await this.usersTokensRepository.create({
+      user_id: user.id,
+      expires_date: refresh_token_expires_date,
+      refresh_token,
     })
 
     const tokenReturn: IResponse = {
@@ -50,6 +73,7 @@ export class AuthenticationUserUseCase {
         name: user.name,
         email: user.email,
       },
+      refresh_token,
     }
 
     return tokenReturn
