b508cc9931445ab22bb44a7751ad22f79c322914
Leandro Lopes <leandro-lopes46@hotmail.com>
2022-01-14 00:45:52 -0300

    feat: add method compareInDays on DayjsDateProvider

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -2,4 +2,5 @@ export interface IDateProvider {
   convertToUTC(date: Date): string
   compareInHours(start_date: Date, end_date: Date): number
   dateNow(): Date
+  compareInDays(start_date: Date, end_date: Date): number
 }
