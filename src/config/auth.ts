f50b488a8394cf4a92af8ac09ab89a934b645ac5
Leandro Lopes <leandro-lopes46@hotmail.com>
2022-01-14 15:50:15 -0300

    feat: add auth config

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export const auth = {
+  secret_token: 'fe275a5908b5650488e0b0342c2d6cc',
+  secret_refresh_token: 'f88dbd560ae640d717ae22e6ed409de7',
+  expires_in_token: '15m',
+  expires_in_refresh_token: '30d',
+  expires_refresh_token_days: 30,
+}
