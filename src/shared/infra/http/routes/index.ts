e8b643c02f03b18d336b1bfdb80647a6f601fd1a
Leandro Lopes <leandro-lopes46@hotmail.com>
2022-01-15 00:25:57 -0300

    feat: add sendForgotPasswordMail use case

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -6,6 +6,7 @@ import { usersRoutes } from './users.routes'
 import { authenticateRoutes } from './authenticate.routes'
 import { carRoutes } from './cars.routes'
 import { rentalRoutes } from './rental.routes'
+import { passwordRoutes } from './password.routes'
 
 export const router = Router()
 
@@ -14,4 +15,5 @@ router.use('/specifications', specificationRoutes)
 router.use('/users', usersRoutes)
 router.use('/cars', carRoutes)
 router.use('/rentals', rentalRoutes)
+router.use('/password', passwordRoutes)
 router.use(authenticateRoutes)
